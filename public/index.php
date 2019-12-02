ff9bb33d95181aec3a72a4da4b1c626963c51f77
Krisztian Papp <fejlesztes@letscode.hu>
2019-12-02 20:57:37 +0100

    php-oop-25

--- public/index.php
+++ public/index.php
@@ -6,7 +6,26 @@ error_reporting(E_ALL);
 ini_set("display_errors", "1");
 
 require_once "../core/functions.php";
-require_once "../autoload.php";
+require_once "../vendor/autoload.php";
+
+// Create the Transport
+$transport = (new Swift_SmtpTransport('smtp.example.org', 25))
+    ->setUsername('your username')
+    ->setPassword('your password')
+;
+
+// Create the Mailer using your created Transport
+$mailer = new Swift_Mailer($transport);
+
+// Create a message
+$message = (new Swift_Message('Wonderful Subject'))
+    ->setFrom(['john@doe.com' => 'John Doe'])
+    ->setTo(['receiver@domain.org', 'other@domain.org' => 'A name'])
+    ->setBody('Here is the message itself')
+;
+
+// Send the message
+$result = $mailer->send($message);
 
 (new \Application(new ServiceContainer(include "../services.php")))->start(realpath(__DIR__. "/../"));
 
